8521dee2120a9d2b5aa58c87146f060ba38ee82a
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-04-11 13:35:08 -0400

    More Regex Work

---
+++ Python-Files/regexBasics.py
@@ -0,0 +1,18 @@
+import re
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print('Phone number found: ' + mo.group())
+
+
+""" or you can try this """
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(0))
+print(mo.group())
+
+""" or pipe characters"""
+heroRegex = re.compile (r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+mo1.group()
