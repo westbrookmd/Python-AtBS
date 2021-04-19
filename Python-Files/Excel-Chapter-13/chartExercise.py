4562b8753493db7be18e0acbd16c52ba14dc98f5
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-04-19 12:29:32 -0400

    Chapter 14 Google Sheets API
    Sheets + Drive API implemented for ezsheets

--- Python-Files/Excel-Chapter-13/chartExercise.py
+++ Python-Files/Excel-Chapter-13/chartExercise.py
@@ -6,6 +6,7 @@ sheet = wb['Sheet']
 #Create the chart
 chartObj = openpyxl.chart.BarChart()
 
+
 #Get data and labels from the worksheet through references
 labels = openpyxl.chart.Reference(sheet, min_col = 1, min_row = 2, max_row = 15)
 data = openpyxl.chart.Reference(sheet, min_col = 2, min_row = 2, max_row = 15)
@@ -18,5 +19,6 @@ chartObj.set_categories(labels)
 chartObj.title = 'Produce Data'
 chartObj.x_axis.title = 'Produce'
 chartObj.y_axis.title = 'Quantity'
+
 sheet.add_chart(chartObj, 'F5')
 wb.save('sampleChart.xlsx')
